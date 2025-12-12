be1d92bd004be08fe352ef86ac9d46e6e5e85ff6
Javlon <apple@MacBook-Pro-Apple.local>
2025-12-12 18:19:42 +0500

    finish

--- src/main.js
+++ src/main.js
@@ -5,64 +5,82 @@ import * as http from "http"
 const PORT = 3_001
 
 const server = http.createServer(async (req, res) => {
-
-
   const url = req.url.split('/').filter(Boolean)
-
-
   console.log(url);
-
   if (url.length == 0) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    return res.end('home page')
+    if (req.method === "GET") {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      return res.end('home page')
+    } else {
+      res.writeHead(405)
+      return res.end()
+    }
   }
 
   if (url[0] === "posts") {
-    const json = await fs.readFile("./Json/posts.json", "utf-8");
+    if (req.method === "GET") {
+      const json = await fs.readFile("./Json/posts.json", "utf-8");
 
-    const parse = JSON.parse(json)
-    const filter = parse.filter(e => e.id == url[1])
+      const parse = JSON.parse(json)
+      const filter = parse.filter(e => e.id == url[1])
 
-    if (filter.length > 0) {
-      res.writeHead(200, { "content-type": "application/json" })
-      res.end(JSON.stringify(filter))
+      if (filter.length > 0) {
+        res.writeHead(200, { "content-type": "application/json" })
+        res.end(JSON.stringify(filter))
+      }
+      res.writeHead(200, { "Content-Type": "application/json" });
+      return res.end(json)
+    } else {
+      res.writeHead(405)
+      return res.end()
     }
-    res.writeHead(200, { "Content-Type": "application/json" });
-    return res.end(json)
   }
 
   if (url[0] === "comments") {
-    const json = await fs.readFile("./Json/comments.json", "utf-8")
+    if (req.method === "GET") {
+      const json = await fs.readFile("./Json/comments.json", "utf-8")
 
-    const parse = JSON.parse(json)
-    const filter = parse.filter((item) => item.id == url[1])
+      const parse = JSON.parse(json)
+      const filter = parse.filter((item) => item.id == url[1])
 
-    if (filter.length > 0) {
+      if (filter.length > 0) {
+        res.writeHead(200, { "content-type": "application/json" })
+        res.end(JSON.stringify(filter))
+      }
       res.writeHead(200, { "content-type": "application/json" })
-      res.end(JSON.stringify(filter))
-    }
-    res.writeHead(200, { "content-type": "application/json" })
 
-    return res.end(json)
+      return res.end(json)
+    } else {
+      res.writeHead(405)
+      return res.end()
+    }
   }
   if (url[0] === "albums") {
-    const json = await fs.readFile("./Json/albums.json", "utf-8")
+    if (req.method === "GET") {
+      const json = await fs.readFile("./Json/albums.json", "utf-8")
 
-    const parse = JSON.parse(json)
-    const filter = parse.filter(el => el.id == url[1])
+      const parse = JSON.parse(json)
+      const filter = parse.filter(el => el.id == url[1])
 
-    if (filter.length > 0) {
+      if (filter.length > 0) {
+        res.writeHead(200, { "content-type": "application/json" })
+        return res.end(JSON.stringify(filter))
+      }
+
       res.writeHead(200, { "content-type": "application/json" })
-      return res.end(JSON.stringify(filter))
+      return res.end(json)
+    } else {
+      res.writeHead(405)
+      return res.end()
     }
-
-    res.writeHead(200, { "content-type": "application/json" })
-    return res.end(json)
 
   }
   res.writeHead(404, "not found", { "content-type": "text/html" })
   return res.end("404 NOT FOUND")
 })
+
+
+
 
 server.listen(PORT, () => {
   console.log(`${PORT} on Port `);
