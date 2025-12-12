08c17ed793d0099535daea55997078fcd0aef437
Javlon <apple@MacBook-Pro-Apple.local>
2025-12-13 02:26:26 +0500

    3-topshiriq

--- src/main.js
+++ src/main.js
@@ -7,6 +7,9 @@ const PORT = 3_001
 const server = http.createServer(async (req, res) => {
   const url = req.url.split('/').filter(Boolean)
   console.log(url);
+
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  
   if (url.length == 0) {
     if (req.method === "GET") {
       res.writeHead(200, { "Content-Type": "text/html" });
@@ -35,6 +38,27 @@ const server = http.createServer(async (req, res) => {
       return res.end()
     }
   }
+
+
+  if (url[0] === "users") {
+    if (req.method === "GET") {
+      const json = await fs.readFile("./Json/users.json", "utf-8");
+
+      const parse = JSON.parse(json)
+      const filter = parse.filter(e => e.id == url[1])
+
+      if (filter.length > 0) {
+        res.writeHead(200, { "content-type": "application/json" })
+        res.end(JSON.stringify(filter))
+      }
+      res.writeHead(200, { "Content-Type": "application/json" });
+      return res.end(json)
+    } else {
+      res.writeHead(405)
+      return res.end()
+    }
+  }
+
 
   if (url[0] === "comments") {
     if (req.method === "GET") {
